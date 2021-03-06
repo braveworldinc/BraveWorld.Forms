ed471b2e559542ffe9e2991deb44586a8998fd17
Daniel Christmas <danieljchristmas@gmail.com>
2021-03-06 14:51:34 -0600

    Create SettingsBase.cs

---
+++ Classes/SettingsBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BraveWorld.Forms.Classes
+{
+    public class SettingsBase
+    {
+
+
+
+
+
+        private static string GenerateEventName(string propertyName) => $"{propertyName}Changed";
+
+        public static void Set(string key, string value)
+        {
+            string eventName = GenerateEventName(key);
+            Preferences.Set(key, value);
+            MessagingCenter.Send(new object(), eventName, key);
+        }
+
+        public static void Set<TSender>(TSender sender, string key, string value, bool showAlert = false) where TSender : Page
+        {
+            string eventName = GenerateEventName(key);
+
+            Preferences.Set(key, value);
+            MessagingCenter.Send(sender, eventName, key);
+
+            if (showAlert)
+                sender.DisplayAlert($"{key} Updated", $"Value: {value}", "Okay");
+        }
+
+
+        public static string Get(string key, string defaultValue = "") => Preferences.Get(key, defaultValue);
+    }
+}
