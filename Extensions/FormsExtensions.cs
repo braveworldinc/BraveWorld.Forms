4d183242247c0bab64457fd22ea0a7f3301b82b3
Daniel Christmas <danieljchristmas@gmail.com>
2021-03-06 14:52:01 -0600

    Created Extensions Namespace

---
+++ Extensions/FormsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BraveWorld.Forms.Extensions
+{
+    public static class FormsExtensions
+    {
+
+        public static async Task ShowExceptionAlert(this Page sender, Exception e)
+        {
+            await sender.DisplayAlert("Exception Thrown", e.Message, "Great...");
+        }
+    }
+}
