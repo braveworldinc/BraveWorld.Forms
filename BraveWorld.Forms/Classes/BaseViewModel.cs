eb5450e5010059b8a8f05276f1e44c4ccf9a4256
Daniel Christmas <danieljchristmas@gmail.com>
2021-04-10 16:18:52 -0500

    New Structure

---
+++ BraveWorld.Forms/Classes/BaseViewModel.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BraveWorld.Forms.Classes
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+        #endregion
+
+        public BaseViewModel()
+        {
+
+        }
+
+        #region INotifyPropertyChanged
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+
+        public async Task DisplayAlert(string title, string message, string cancel)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+        }
+
+        public async Task DisplayAlert(string title, string message, string accept, string cancel = "Cancel")
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        }
+
+        public static async Task ShowException(Exception e)
+        {
+            await Application.Current.MainPage.DisplayAlert("Exception Thrown", e.Message, "Great...");
+        }
+    }
+
+    public class BaseViewModel<TDataService> : INotifyPropertyChanged
+        where TDataService : BaseDataService
+    {
+        protected TDataService DataService;
+
+        #region Properties
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                SetProperty(ref _isBusy, value);
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+        public bool IsNotBusy => !IsBusy;
+
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+        #endregion
+
+        public BaseViewModel()
+        {
+            DataService = DependencyService.Get<TDataService>();
+        }
+
+
+        #region INotifyPropertyChanged
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+
+        public async Task DisplayAlert(string title, string message, string cancel)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+        }
+
+        public async Task DisplayAlert(string title, string message, string accept, string cancel = "Cancel")
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        }
+
+        public static async Task ShowException(Exception e)
+        {
+            await Application.Current.MainPage.DisplayAlert("Exception Thrown", e.Message, "Lovely");
+        }
+
+        public static async Task ShowException(DataServiceRequestException e)
+        {
+            await Application.Current.MainPage.DisplayAlert($"Exception Thrown: {e.StatusCode}", e.Message, "Lovely");
+        }
+    }
+}
---
+++ BraveWorld.Forms/Library.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BraveWorld.Forms
+{
+    /// <summary>
+    ///     A static class to initialize BraveWorld.Forms Library
+    /// </summary>
+    public static class Library
+    {
+        public const string Name = "BraveWorld.Forms";
+
+
+        /// <summary>
+        ///     Initializes the BraveWorld.Forms Library, enables custom namespace http://braveworld.forms.com to be used in XAML.
+        ///     <remarks>This should be called once per application, typically in the `App.xaml.cs` constructor. </remarks>
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        public static void Initialize()
+        {
+        }
+
+        /// <summary>
+        ///     A static class used to enable features that are in preview.
+        /// </summary>
+        public static class PreviewFeatures
+        {
+            /// <summary>
+            ///     Toggles animations for the badge on the <see cref="MenuButton" />
+            /// </summary>
+            //public static bool MenuButtonAnimations { get; private set; }
+
+            /// <summary>
+            ///     Toggles usage of SkeletonView <see cref="SkeletonView" />
+            /// </summary>
+            //public static bool SkeletonView { get; private set; }
+
+            public static bool TestPreviewFeature { get; private set; }
+
+            /// <summary>
+            ///     Enable a feature that's in preview.
+            /// </summary>
+            /// <param name="previewFeature">A string specifying which preview feature you want to enable.</param>
+            public static void EnableFeature(string previewFeature)
+            {
+                //if (previewFeature == nameof(MenuButtonAnimations))
+                //    MenuButtonAnimations = true;
+
+                //if (previewFeature == nameof(SkeletonView))
+                //    SkeletonView = true;
+
+                if (previewFeature == nameof(TestPreviewFeature))
+                    TestPreviewFeature = true;
+            }
+        }
+    }
+}
